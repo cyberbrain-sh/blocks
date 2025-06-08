a6b65f9d5e97a4496851c1d22e16fe1deb5f3237
CyberBrain Contrib <contributing@cyberbrain.sh>
2025-06-08 17:53:53 +0200

    added new statuses

--- lifecycle.go
+++ lifecycle.go
@@ -3,23 +3,29 @@ package blocks
 type LifecycleStatus string
 
 const (
-	LifecycleStatusIngested         = LifecycleStatus("ingested")
-	LifecycleStatusCreated          = LifecycleStatus("created")
-	LifecycleStatusEnriching        = LifecycleStatus("enriching")
-	LifecycleStatusEnriched         = LifecycleStatus("enriched")
-	LifecycleStatusRouted           = LifecycleStatus("routed")
-	LifecycleStatusRoutedFinal      = LifecycleStatus("routed_final")
-	LifecycleStatusEditing          = LifecycleStatus("editing")
-	LifecycleStatusEdited           = LifecycleStatus("edited")
-	LifecycleStatusProcessing       = LifecycleStatus("processing")
-	LifecycleStatusProcessed        = LifecycleStatus("processed")
-	LifecycleStatusIndexing         = LifecycleStatus("indexing")
-	LifecycleStatusIndexed          = LifecycleStatus("indexed")
-	LifecycleStatusOnHold           = LifecycleStatus("on_hold")
-	LifecycleStatusEnrichmentFailed = LifecycleStatus("enrichment_failed")
-	LifecycleStatusRoutingFailed    = LifecycleStatus("routing_failed")
-	LifecycleStatusProcessingFailed = LifecycleStatus("processing_failed")
-	LifecycleStatusArchived         = LifecycleStatus("archived")
+	LifecycleStatusIngested             = LifecycleStatus("ingested")
+	LifecycleStatusCreated              = LifecycleStatus("created")
+	LifecycleStatusEnriching            = LifecycleStatus("enriching")
+	LifecycleStatusEnriched             = LifecycleStatus("enriched")
+	LifecycleStatusRouted               = LifecycleStatus("routed")
+	LifecycleStatusRoutedFinal          = LifecycleStatus("routed_final")
+	LifecycleStatusEditing              = LifecycleStatus("editing")
+	LifecycleStatusEdited               = LifecycleStatus("edited")
+	LifecycleStatusProcessing           = LifecycleStatus("processing")
+	LifecycleStatusProcessed            = LifecycleStatus("processed")
+	LifecycleStatusIndexing             = LifecycleStatus("indexing")
+	LifecycleStatusIndexed              = LifecycleStatus("indexed")
+	LifecycleStatusOnHold               = LifecycleStatus("on_hold")
+	LifecycleStatusEnrichmentFailed     = LifecycleStatus("enrichment_failed")
+	LifecycleStatusRoutingFailed        = LifecycleStatus("routing_failed")
+	LifecycleStatusProcessingFailed     = LifecycleStatus("processing_failed")
+	LifecycleStatusArchived             = LifecycleStatus("archived")
+	LifecycleStatusPreProcessing        = LifecycleStatus("pre_processing")
+	LifecycleStatusPreProcessingFailed  = LifecycleStatus("pre_processing_failed")
+	LifecycleStatusPreProcessed         = LifecycleStatus("pre_processed")
+	LifecycleStatusPostProcessing       = LifecycleStatus("post_processing")
+	LifecycleStatusPostProcessingFailed = LifecycleStatus("post_processing_failed")
+	LifecycleStatusPostProcessed        = LifecycleStatus("post_processed")
 )
 
 // String returns the string representation of the lifecycle status.
@@ -30,7 +36,15 @@ func (s LifecycleStatus) String() string {
 
 func (s LifecycleStatus) Recordable() bool {
 	switch s {
-	case LifecycleStatusIngested, LifecycleStatusCreated, LifecycleStatusEnriched, LifecycleStatusRouted, LifecycleStatusRoutedFinal, LifecycleStatusEdited, LifecycleStatusProcessed:
+	case LifecycleStatusIngested,
+		LifecycleStatusCreated,
+		LifecycleStatusEnriched,
+		LifecycleStatusRouted,
+		LifecycleStatusRoutedFinal,
+		LifecycleStatusEdited,
+		LifecycleStatusProcessed,
+		LifecycleStatusPreProcessed,
+		LifecycleStatusPostProcessed:
 		return true
 	default:
 		return false
